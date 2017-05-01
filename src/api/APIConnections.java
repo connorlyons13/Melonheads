33509f9b71ebca46b323939dae8176ac9932ff22
nickschillaci1 <nickschillaci1@gmail.com>
2017-04-30 22:50:51 -0400

    added the ability to retrieve recent songs and playlists from the server

--- src/api/APIConnections.java
+++ src/api/APIConnections.java
@@ -237,6 +237,23 @@ public class APIConnections {
 		}
 	}
     
+    public static ArrayList<Integer> getRecentSongIds() throws Exception {
+      StringBuilder result = new StringBuilder();
+      URL url = new URL(address + "/songs/recent");
+      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+      conn.setRequestMethod("GET");
+      BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+      String line;
+      while ((line = rd.readLine()) != null) {
+         result.append(line);
+      }
+      rd.close();
+      
+      // parse json array to ArrayList
+      ArrayList<Integer> songIds = new Gson().fromJson(result.toString(), new TypeToken<ArrayList<Integer>>(){}.getType());
+      return songIds;
+    }
+        
     public static String getYoutubeVideoId(String youtubeUrl) {
         String video_id="";
         if (youtubeUrl != null && youtubeUrl.trim().length() > 0 && youtubeUrl.startsWith("http")) {
@@ -254,7 +271,7 @@ public class APIConnections {
     }
     
     /**
-     * Test Methods for Playlist Endpoints
+     * Methods for Playlist Endpoints
      */
     public static ArrayList<Playlist> getPlaylist(String getID) throws Exception {
         StringBuilder result = new StringBuilder();
@@ -372,6 +389,23 @@ public class APIConnections {
 		} else {
 	            return false;  
 		}  
+    }
+    
+    public static ArrayList<Integer> getRecentPlaylistIds() throws Exception {
+      StringBuilder result = new StringBuilder();
+      URL url = new URL(address + "/playlists/recent");
+      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+      conn.setRequestMethod("GET");
+      BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+      String line;
+      while ((line = rd.readLine()) != null) {
+         result.append(line);
+      }
+      rd.close();
+      
+      // parse json array to ArrayList
+      ArrayList<Integer> playlistIds = new Gson().fromJson(result.toString(), new TypeToken<ArrayList<Integer>>(){}.getType());
+      return playlistIds;
     }
     
 }
